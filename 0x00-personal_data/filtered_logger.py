67601cd853a8600767d95295a537b83701442a19
vickouma77 <Vickprestone20@gmail.com>
2023-08-02 23:16:10 +0300

    filtered_logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -8,6 +8,7 @@ from typing import List
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
-    pattern = '|'.join(fields)
-    replacement = f'\\1={redaction}{separator}'
-    return re.sub(rf'({pattern})=.*?{separator}', replacement, message)
+    for field in fields:
+        message = re.sub(f"{field}=.+?{separator}",
+                         f"{field}={redaction}{separator}", message)
+    return message
