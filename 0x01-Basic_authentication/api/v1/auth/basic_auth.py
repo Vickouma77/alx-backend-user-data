a71beb636be0345614e56ee583d5c793ef7d00a0
vickouma77 <Vickprestone20@gmail.com>
2023-08-08 16:01:03 +0300

    update Basic Auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,3 +9,16 @@ class BasicAuth(Auth):
     """
     BasicAuth class
     """
+
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """
+        returns the Base64 part of the Authorization header
+        """
+        if authorization_header is None:
+            return None
+        if type(authorization_header) is not str:
+            return None
+        if authorization_header[:6] != "Basic ":
+            return None
+        return authorization_header[6:]
