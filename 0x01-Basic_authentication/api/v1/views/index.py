deee4d68b9bcc2c326ae9644538ad36d1062537b
vickouma77 <Vickprestone20@gmail.com>
2023-08-08 12:18:10 +0300

    Forbiden handler: Unauthorized for api/v1/index.py

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -33,3 +33,12 @@ def unauthorized() -> str:
       - abort(401)
     """
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+    Return:
+      - abort(403)
+    """
+    abort(403)
