95a04652a2f3fa88fcc9e150f5d1b697cf994fd3
vickouma77 <Vickprestone20@gmail.com>
2023-08-08 18:20:32 +0300

    update Basic Auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -83,3 +83,28 @@ class BasicAuth(Auth):
         for user in users:
             if user.is_valid_password(user_pwd):
                 return user
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """overloads Auth and retrieves the User instance for a request
+        request:
+            - the request
+        Return:
+            - the User instance
+        """
+        auth_header = self.authorization_header(request)
+        if auth_header is None:
+            return None
+        base64_auth_header = self.extract_base64_authorization_header(
+            auth_header)
+        if base64_auth_header is None:
+            return None
+        decoded_auth_header = self.decode_base64_authorization_header(
+            base64_auth_header)
+        if decoded_auth_header is None:
+            return None
+        user_credentials = self.extract_user_credentials(decoded_auth_header)
+        if user_credentials is None:
+            return None
+        user = self.user_object_from_credentials(
+            user_credentials[0], user_credentials[1])
+        return user
