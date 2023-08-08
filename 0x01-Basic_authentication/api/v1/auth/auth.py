33efc78c63c5a0e0c309356ad8a3f3e87b18d7f4
vickouma77 <Vickprestone20@gmail.com>
2023-08-08 18:26:11 +0300

    update Auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -24,7 +24,7 @@ class Auth:
         if excluded_paths is None or len(excluded_paths) == 0:
             return True
         if path[-1] != '/':
-            path += '/'.split('*')[-1]
+            path += '/'
         if path in excluded_paths:
             return False
         return True
