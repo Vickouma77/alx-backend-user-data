1e877867a1373d1b8c8ea792e7abd8294fcb6415
vickouma77 <Vickprestone20@gmail.com>
2023-08-08 13:00:35 +0300

    pycodestyle compliant

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,21 +11,18 @@ class Auth:
     """ Auth class
     """
 
-
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Require auth
         """
         if path is None:
             return True
-        
-    
+
     def authorization_header(self, request=None) -> str:
         """Authorization header
         """
         if request is None:
             return None
         return request.headers.get('Authorization', None)
-    
 
     def current_user(self, request=None) -> TypeVar('User'):
         """Current user
