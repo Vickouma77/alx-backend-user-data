bc3c5a214fe49ef67005d088f4c2d43237ef1f1b
vickouma77 <Vickprestone20@gmail.com>
2023-08-08 12:53:22 +0300

    updating Auth class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,16 +11,12 @@ class Auth:
     """ Auth class
     """
 
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Require auth
         """
-        if path is None or excluded_paths is None or excluded_paths == []:
+        if path is None:
             return True
-        if path[-1] != '/':
-            path += '/'
-        if path in excluded_paths:
-            return False
-        return True
         
     
     def authorization_header(self, request=None) -> str:
