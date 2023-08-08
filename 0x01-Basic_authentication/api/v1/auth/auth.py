f99935bda8ecfa9be453204bb5d33ff77743d811
vickouma77 <Vickprestone20@gmail.com>
2023-08-08 12:44:09 +0300

    implementing auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -15,10 +15,13 @@ class Auth():
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Require auth
         """
-        if path is None:
+        if path is None or excluded_paths is None or excluded_paths == []:
             return True
         if path[-1] != '/':
-            return path + '/'
+            path += '/'
+        if path in excluded_paths:
+            return False
+        return True
         
     
     def authorization_header(self, request=None) -> str:
