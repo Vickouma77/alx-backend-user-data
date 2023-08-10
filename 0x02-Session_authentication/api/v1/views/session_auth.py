f5484345553534f2c3384b2ce028bbaec903c8bb
vickouma77 <Vickprestone20@gmail.com>
2023-08-10 23:03:22 +0300

    New view for Session Authentication

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -3,10 +3,9 @@
 Module of SessionAuth views
 """
 from api.v1.views import app_views
-from flask import abort, jsonify, request
+from flask import jsonify, request
 from models.user import User
-from os import getenv
-from api.v1.app import auth
+import os
 
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
@@ -16,20 +15,19 @@ def login() -> str:
     """
     email = request.form.get('email')
     password = request.form.get('password')
-    if email is None or email == "":
+    if email is None or email == '':
         return jsonify({"error": "email missing"}), 400
-    if password is None or password == "":
+    if password is None or password == '':
         return jsonify({"error": "password missing"}), 400
-    try:
-        user = User.search({'email': email})
-    except Exception:
+    users = User.search({"email": email})
+    if not users or users == []:
         return jsonify({"error": "no user found for this email"}), 404
-    if not user:
-        return jsonify({"error": "no user found for this email"}), 404
-    if not user[0].is_valid_password(password):
-        return jsonify({"error": "wrong password"}), 401
-    from api.v1.app import auth
-    session_id = auth.create_session(user[0].id)
-    user_json = jsonify(user[0].to_json())
-    user_json.set_cookie(getenv('SESSION_NAME'), session_id)
-    return user_json
+    for user in users:
+        if user.is_valid_password(password):
+            from api.v1.app import auth
+            session_id = auth.create_session(user.id)
+            resp = jsonify(user.to_json())
+            session_name = os.getenv('SESSION_NAME')
+            resp.set_cookie(session_name, session_id)
+            return resp
+    return jsonify({"error": "wrong password"}), 401
