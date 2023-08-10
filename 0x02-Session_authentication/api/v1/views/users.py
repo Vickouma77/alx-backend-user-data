7202bc7ebd2256d0c21b66dcb028c054abbff511
vickouma77 <Vickprestone20@gmail.com>
2023-08-10 11:54:13 +0300

    update user.py

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -25,13 +25,11 @@ def view_one_user(user_id: str = None) -> str:
       - User object JSON represented
       - 404 if the User ID doesn't exist
     """
-    if user_id is None or user_id == "me" and request.current_user is None:
+    if user_id is None:
         abort(404)
     user = User.get(user_id)
     if user is None:
         abort(404)
-    if user_id == "me" and request.current_user is not None:
-        return jsonify(request.current_user.to_json())
     return jsonify(user.to_json())
 
 
