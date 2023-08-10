1c1df63ce02e9600e518d31819a23e04996f622e
vickouma77 <Vickprestone20@gmail.com>
2023-08-10 12:12:02 +0300

    update app.py

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -61,6 +61,7 @@ def before_request_func() -> str:
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
+    request.current_user = auth.current_user(request)
 
 
 if __name__ == "__main__":
