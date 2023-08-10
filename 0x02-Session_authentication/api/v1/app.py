c905d897b8eef50fc2af5508b8c86f776ad10c1d
vickouma77 <Vickprestone20@gmail.com>
2023-08-10 22:41:34 +0300

    Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -67,8 +67,9 @@ def before_request_func() -> str:
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
-    if auth.authorization_header(request) is None and\
-            auth.session_cookie(request) is None:
+    if auth.authorization_header(request) is None:
+        abort(401)
+    if auth.session_cookie(request) is None:
         abort(401)
     request.current_user = auth.current_user(request)
 
