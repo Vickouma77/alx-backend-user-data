2a1f4c3ba6e99bd04984403f234a13c494c8b60c
vickouma77 <Vickprestone20@gmail.com>
2023-08-10 20:13:28 +0300

    updating session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -25,14 +25,3 @@ class SessionAuth(Auth):
         session_id = str(uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
-
-    def user_id_for_session_id(self, session_id: str = None) -> str:
-        """
-        user_id based on session_id:
-        return:
-            none if session_id is None
-            none if session_id is not a string
-        """
-        if session_id is None or type(session_id) != str:
-            return None
-        return self.user_id_by_session_id.get(session_id)
