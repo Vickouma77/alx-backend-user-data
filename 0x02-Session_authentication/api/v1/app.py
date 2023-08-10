62fa64dedc7175953b6256c07738707acc732641
vickouma77 <Vickprestone20@gmail.com>
2023-08-10 11:56:32 +0300

    update app.py

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -62,9 +62,6 @@ def before_request_func() -> str:
     if auth.current_user(request) is None:
         abort(403)
 
-    request.current_user = auth.current_user(request)
-
-
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
