3f1a5107164d2f71720fd93fbc5351df5620d0b1
vickouma77 <Vickprestone20@gmail.com>
2023-08-10 23:20:44 +0300

    Logout

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,51 +1,71 @@
 #!/usr/bin/env python3
 """
-Module of Session Authentication
+Definition of class SessionAuth
 """
-
-from api.v1.auth.auth import Auth
+import base64
 from uuid import uuid4
+from typing import TypeVar
+
+from .auth import Auth
+from models.user import User
 
 
 class SessionAuth(Auth):
-    """
-    SessionAuth class
+    """ Implement Session Authorization protocol methods
     """
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
         """
-        Create session for us_id:
-        return:
-             none if user_id is None
-             none if user_id is not a string
+        Creates a Session ID for a user with id user_id
+        Args:
+            user_id (str): user's user id
+        Return:
+            None is user_id is None or not a string
+            Session ID in string format
         """
-        if user_id is None or type(user_id) != str:
+        if user_id is None or not isinstance(user_id, str):
             return None
-        session_id = str(uuid4())
-        self.user_id_by_session_id[session_id] = user_id
-        return session_id
+        id = uuid4()
+        self.user_id_by_session_id[str(id)] = user_id
+        return str(id)
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+        Returns a user ID based on a session ID
+        Args:
+            session_id (str): session ID
+        Return:
+            user id or None if session_id is None or not a string
+        """
+        if session_id is None or not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id)
 
     def current_user(self, request=None):
-        """ (overload) that returns a User instance
-          based on a cookie value:
         """
-        session_id = self.session_cookie(request)
-        user_id = self.user_id_by_session_id.get(session_id, None)
-        from models.user import User
-        return User.get(user_id)
-    
+        Return a user instance based on a cookie value
+        Args:
+            request : request object containing cookie
+        Return:
+            User instance
+        """
+        session_cookie = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_cookie)
+        user = User.get(user_id)
+        return user
+
     def destroy_session(self, request=None):
-        """ (overload) that deletes the user session / logout
-          to display the welcome page
+        """
+        Deletes a user session
         """
         if request is None:
             return False
-        session_id = self.session_cookie(request)
-        if session_id is None:
+        session_cookie = self.session_cookie(request)
+        if session_cookie is None:
             return False
-        user_id = self.user_id_by_session_id.get(session_id, None)
+        user_id = self.user_id_for_session_id(session_cookie)
         if user_id is None:
             return False
-        del self.user_id_by_session_id[session_id]
+        del self.user_id_by_session_id[session_cookie]
         return True
