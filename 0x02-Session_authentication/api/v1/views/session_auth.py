f8c7d3d771649f500cf0dfd20f92d4b1d07ef4dc
vickouma77 <Vickprestone20@gmail.com>
2023-08-10 23:19:38 +0300

    Logout

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -10,33 +10,37 @@ from models.user import User
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def auth_session():
     """
-    POST /api/v1/auth_session/login
+    Handle user login
+    Return:
+        dictionary representation of user if found else error message
     """
     email = request.form.get('email')
     password = request.form.get('password')
-    if not email:
+    if email is None or email == '':
         return jsonify({"error": "email missing"}), 400
-    if not password:
+    if password is None or password == '':
         return jsonify({"error": "password missing"}), 400
-    user = User.search({'email': email})
-    if not user:
+    users = User.search({"email": email})
+    if not users or users == []:
         return jsonify({"error": "no user found for this email"}), 404
-    if not user[0].is_valid_password(password):
-        return jsonify({"error": "wrong password"}), 401
-    from api.v1.app import auth
-    session_id = auth.create_session(user[0].id)
-    res = jsonify(user[0].to_json())
-    res.set_cookie(os.getenv('SESSION_NAME'), session_id)
-    return res
+    for user in users:
+        if user.is_valid_password(password):
+            from api.v1.app import auth
+            session_id = auth.create_session(user.id)
+            resp = jsonify(user.to_json())
+            session_name = os.getenv('SESSION_NAME')
+            resp.set_cookie(session_name, session_id)
+            return resp
+    return jsonify({"error": "wrong password"}), 401
 
 
 @app_views.route('/auth_session/logout', methods=['DELETE'],
                  strict_slashes=False)
-def auth_session_logout():
+def handle_logout():
     """
-    DELETE /api/v1/auth_session/logout
+    Handle user logout
     """
     from api.v1.app import auth
-    if auth.destroy_session(request) is False:
-        abort(404)
-    return jsonify({}), 200
+    if auth.destroy_session(request):
+        return jsonify({}), 200
+    abort(404)
