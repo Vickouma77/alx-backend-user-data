2aac0efa8d0d2c8b3c576619d1ad5f3797db7096
vickouma77 <Vickprestone20@gmail.com>
2023-08-10 23:06:32 +0300

    New view for Session Authentication

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
+""" Module of Users views
 """
-Module of SessionAuth views
-"""
-from api.v1.views import app_views
-from flask import jsonify, request
-from models.user import User
 import os
+from flask import jsonify, request
+from api.v1.views import app_views
+from models.user import User
 
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
-def login() -> str:
+def auth_session():
     """
     POST /api/v1/auth_session/login
     """
