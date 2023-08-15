a9dd322ac50eb2cf9f91fd6ff4d47dc858d6d9fe
vickouma77 <Vickprestone20@gmail.com>
2023-08-15 22:26:50 +0300

    implementing class Auth register user method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,6 +4,8 @@ Hash password
 """
 
 from bcrypt import hashpw, gensalt
+from sqlalchemy.orm.exc import NoResultFound
+from db import DB
 
 
 def _hash_password(password: str) -> bytes:
@@ -11,3 +13,21 @@ def _hash_password(password: str) -> bytes:
     Hash password
     """
     return hashpw(password.encode('utf-8'), gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        Register user
+        """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f'User {email} already exists')
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
