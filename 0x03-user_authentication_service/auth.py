1c27a1185f6f7d8e025b3bc5612fa952e51bbbf7
vickouma77 <Vickprestone20@gmail.com>
2023-08-15 23:00:39 +0300

    Credentials validation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -32,3 +32,13 @@ class Auth:
             raise ValueError(f'User {email} already exists')
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
+        
+    def valid_login(self, email: str, password: str) -> bool:
+        """
+        Valid login
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            return hashpw(password.encode('utf-8'), user.hashed_password) == user.hashed_password
+        except NoResultFound:
+            return False
