8fbaa27882d886851d88e7e743e05219ae411b1d
vickouma77 <Vickprestone20@gmail.com>
2023-08-15 21:26:51 +0300

    Find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -41,4 +41,7 @@ class DB:
     def find_user_by(self, **kwargs) -> User:
         """Method that returns the first row found in the users table
         """
-        return self._session.query(User).filter_by(**kwargs).first()
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if user is None:
+            raise NoResultFound
+        return user
