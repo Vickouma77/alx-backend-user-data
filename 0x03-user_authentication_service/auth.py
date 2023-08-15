3fb56b1a30f8e5edf5feb74266cbc43db46ef412
vickouma77 <Vickprestone20@gmail.com>
2023-08-15 23:12:51 +0300

    Credentials validation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,7 +3,7 @@
 Hash password
 """
 
-from bcrypt import hashpw, gensalt
+from bcrypt import hashpw, gensalt, checkpw
 from sqlalchemy.orm.exc import NoResultFound
 from user import User
 from db import DB
@@ -39,6 +39,6 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(email=email)
-            return hashpw(password.encode('utf-8'), user.hashed_password) == user.hashed_password
+            return checkpw(password.encode('utf-8'), user.hashed_password)
         except NoResultFound:
             return False
