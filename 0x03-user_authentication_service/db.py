96a60849b3c07bbca9ab0d3ae17742e864920e62
vickouma77 <Vickprestone20@gmail.com>
2023-08-15 20:07:16 +0300

    removing whitespace line 33

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -30,7 +30,7 @@ class DB:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
-    
+
     def add_user(self, email: str, hashed_password: str) -> User:
         """Method that saves a new user to the database
         """
