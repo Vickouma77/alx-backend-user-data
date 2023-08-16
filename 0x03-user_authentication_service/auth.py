14e53de75ccdeef8eb70bf619affd62121cff393
vickouma77 <Vickprestone20@gmail.com>
2023-08-16 21:50:19 +0300

    Log in

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -62,3 +62,15 @@ class Auth:
             return session_id
         except NoResultFound:
             return None
+
+    def get_user_from_session_id(self, session_id: str) -> str:
+        """
+        Get user from session id
+        """
+        if session_id is None or not isinstance(session_id, str):
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
+            return None
