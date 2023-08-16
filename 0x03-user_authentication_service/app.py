a3602c9cd26266eb4ff1e2b38d67866dbcce1442
vickouma77 <Vickprestone20@gmail.com>
2023-08-16 21:29:20 +0300

    Log in

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -35,9 +35,9 @@ def users() -> str:
         return jsonify({"message": "email already registered"}), 400
 
 
-@app.route('/sessions', methods=['POST'], strict_slashes=False)
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
 def login() -> str:
-    """ POST /sessions
+    """POST /sessions
     Return:
       - JSON payload
     """
@@ -46,8 +46,9 @@ def login() -> str:
 
     if not Auth.valid_login(email, password):
         abort(401)
+
     session_id = Auth.create_session(email)
-    response = jsonify({"email": email, "message": "logged in"})
+    response = jsonify({"email": f"{email}", "message": "logged in"})
     response.set_cookie("session_id", session_id)
     return response
 
